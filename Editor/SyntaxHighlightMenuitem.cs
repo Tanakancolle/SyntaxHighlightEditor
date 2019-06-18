e0c57e63a235dfffe41d25e8454634ca3fc0dc2c
Shinogi <shinogi_kazuki@cyberagent.co.jp>
2019-06-18 19:03:13 +0900

    MenuItem削除

--- Editor/SyntaxHighlightMenuitem.cs
+++ Editor/SyntaxHighlightMenuitem.cs
@@ -12,7 +12,7 @@ namespace SyntaxHighlightEditor
         private const string MenuItemPrefix = "Tools/Syntax Highlight Editor/";
         private const int OriginalPriorityNumber = 2000;
 
-        [MenuItem(MenuItemPrefix + "Reset CSharp Editor", false, OriginalPriorityNumber + (int) Priority.ResetCSharpEditor)]
+        //[MenuItem(MenuItemPrefix + "Reset CSharp Editor", false, OriginalPriorityNumber + (int) Priority.ResetCSharpEditor)]
         public static void ResetCSharpEditor()
         {
             SyntaxHighlightUtility.ResetCSharpEditor();
